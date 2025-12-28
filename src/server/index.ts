124a68d9c0ec867b7e46dda60e15cbc86e83fc96
pderyuga <polina.deryuga@persuit.com>
2025-12-28 13:41:58 -0500

    feat: connect peril to rabbitmq

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,24 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+
+  const connectionString = "amqp://guest:guest@localhost:5672/";
+  const rabbitMq = await amqp.connect(connectionString);
+  console.log("Connection to RabbitMQ was successful!");
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await rabbitMq.close();
+        console.log("\nRabbitMQ connection closed");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection: ", err);
+      } finally {
+        process.exit(0);
+      }
+    })
+  );
 }
 
 main().catch((err) => {
