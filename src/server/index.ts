38a7e138bee123c3bc6192e29eca8b4b98af7e9b
pderyuga <polina.deryuga@persuit.com>
2025-12-29 17:11:55 -0500

    feat: update the server so it can interactively pause and resume the game

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 import type { PlayingState } from "../internal/gamelogic/gamestate.js";
+import { printServerHelp, getInput } from "../internal/gamelogic/gamelogic.js";
 
 async function main() {
   console.log("Starting Peril server...");
@@ -9,6 +10,7 @@ async function main() {
   const connectionString = "amqp://guest:guest@localhost:5672/";
   const rabbitMq = await amqp.connect(connectionString);
   console.log("Connection to RabbitMQ was successful!");
+  printServerHelp();
 
   ["SIGINT", "SIGTERM"].forEach((signal) =>
     process.on(signal, async () => {
@@ -25,11 +27,51 @@ async function main() {
 
   const confirmChannel = await rabbitMq.createConfirmChannel();
 
-  try {
-    const data: PlayingState = { isPaused: true };
-    await publishJSON(confirmChannel, ExchangePerilDirect, PauseKey, data);
-  } catch (err) {
-    console.error("Error publishing message: ", err);
+  while (true) {
+    // do stuff infintely
+    const result = await getInput();
+    if (!result.length) {
+      continue;
+    }
+
+    if (result[0] === "pause") {
+      console.log("Sending pause message...");
+      try {
+        const pauseMessage: PlayingState = { isPaused: true };
+        await publishJSON(
+          confirmChannel,
+          ExchangePerilDirect,
+          PauseKey,
+          pauseMessage
+        );
+      } catch (err) {
+        console.error("Error publishing pause message: ", err);
+      }
+    }
+
+    if (result[0] === "resume") {
+      console.log("Sending resume message...");
+      try {
+        const resumeMessage: PlayingState = { isPaused: false };
+        await publishJSON(
+          confirmChannel,
+          ExchangePerilDirect,
+          PauseKey,
+          resumeMessage
+        );
+      } catch (err) {
+        console.error("Error publishing resume message: ", err);
+      }
+    }
+
+    if (result[0] === "quit") {
+      console.log("Exiting...");
+      break;
+    }
+
+    if (result[0] && !["pause", "resume", "quit"].includes(result[0])) {
+      console.log(`Unknown command: ${result}`);
+    }
   }
 }
 
